abf5dab93446cba76d1332e24c84ea2ec2be0e22
m6c93 <mohamed6c@gmail.com>
2026-09-13 15:30:32 +0200

    Affiche les arbres entiers dans les parcelles

--- applications/coran/coran.mjs
+++ applications/coran/coran.mjs
@@ -1,6 +1,6 @@
 import {RECITER,STEPS,STARTER,GOALS,cleanProgress,validRef,nextRef,createSession,completeVerse,earnTree,matches,parentPlan,splitBasmala} from './model.mjs';
 import {QuranPlayer} from './audio.mjs?v=2';
-import {createGarden} from './juz-garden.mjs?v=3';
+import {createGarden} from './juz-garden.mjs?v=4';
 let grove=null;
 import {mountParallax} from './motion.mjs';
 let disposeMotion=()=>{};let bookMode=false,nightMode=false,familyTurn=0,familyTogetherRepeat=3,familyReady=false,pendingFamily=null;
